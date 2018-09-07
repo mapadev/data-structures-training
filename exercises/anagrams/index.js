586a7af583a56629301cf7ccea96fc529bd11d61
gogolab <m4ttek@gmail.com>
2018-09-07 17:01:34 +0200

    finished anagrams excercise

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,53 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+// function anagrams(stringA, stringB) {
+//     return clean(stringA) === clean(stringB);
+// }
+
+// function clean(str) {
+//     return str
+//         .toLowerCase()
+//         .split("")
+//         .filter(char => {
+//             return /[a-z]/.test(char);
+//         })
+//         .sort()
+//         .join("");
+// }
+
+function anagrams(stringA, stringB) {
+    const cleanA = cleanStr(stringA);
+    const cleanB = cleanStr(stringB);
+
+    const aCharMap = buildCharMap(cleanA.split(""));
+    const bCharMap = buildCharMap(cleanB.split(""));
+
+    if (cleanA.length !== cleanB.length) {
+        return false;
+    }
+
+    for (let key in aCharMap) {
+        if (aCharMap[key] !== bCharMap[key]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function cleanStr(str) {
+    return str.toLowerCase().replace(/\W/g, "");
+}
+
+function buildCharMap(arr) {
+    const charMap = {};
+
+    arr.forEach(char => {
+        charMap[char] = charMap[char] + 1 || 1;
+    });
+
+    return charMap;
+}
 
 module.exports = anagrams;
