5466e7a2dc55b9b7a5f4a4ecb4e10614fca29919
gogolab <m4ttek@gmail.com>
2018-09-08 14:00:38 +0200

    finished steps excercise

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,66 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//     for (let i = 1; i <= n; i++) {
+//         step =
+//             new Array(i).fill("#").join("") +
+//             new Array(n - i).fill(" ").join("");
+//         console.log(step);
+//     }
+// }
+
+// function steps(n) {
+//     for (let row = 0; row < n; row++) {
+//         let stair = "";
+
+//         for (let col = 0; col < n; col++) {
+//             if (col <= row) {
+//                 stair += "#";
+//             } else {
+//                 stair += " ";
+//             }
+//         }
+
+//         console.log(stair);
+//     }
+// }
+
+// function steps(n, row = 0, string = "") {
+//     if (n === row) {
+//         return;
+//     }
+
+//     if (string.length < n) {
+//         if (string.length <= row) {
+//             string += "#";
+//         } else {
+//         string += " ";
+//         }
+//         return steps(n, row, string);
+//     }
+
+//     console.log(string);
+
+//     steps(n, row + 1);
+// }
+
+function steps(n, row = 0, string = "") {
+    if (n === row) {
+        return;
+    }
+
+    if (n === string.length) {
+        console.log(string);
+        return steps(n, row + 1);
+    }
+
+    if (string.length <= row) {
+        string += "#";
+    } else {
+        string += " ";
+    }
+    return steps(n, row, string);
+}
 
 module.exports = steps;
