cd1e99bc2c39dfb97b047b23b4c3236bbd5bb324
gogolab <m4ttek@gmail.com>
2018-09-10 16:15:29 +0200

    finished vowels excersise

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,25 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//     const vowels = str.split("").filter(char => {
+//         return /[aeiou]/i.test(char);
+//     });
+
+//     return vowels.length;
+// }
+
+function vowels(str) {
+    const validator = ["a", "e", "i", "o", "u"];
+    let result = "";
+
+    for (let char of str.toLowerCase()) {
+        if (validator.includes(char)) {
+            result += char;
+        }
+    }
+
+    return result.length;
+}
 
 module.exports = vowels;
