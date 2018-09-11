d98e8b7ab6de5c18ee4438135eb961208f01a522
gogolab <m4ttek@gmail.com>
2018-09-11 16:50:53 +0200

    finished matrix excersise

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,45 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let startRow = 0;
+    let startCol = 0;
+    let endRow = n - 1;
+    let endCol = n - 1;
+    const results = [];
+    counter = 1;
+
+    for (let i = 0; i < n; i++) {
+        results.push([]);
+    }
+
+    while (startRow <= endRow && startCol <= endCol) {
+        for (let col = startCol; col <= endCol; col++) {
+            results[startRow][col] = counter;
+            counter++;
+        }
+        startRow++;
+
+        for (let row = startRow; row <= endRow; row++) {
+            results[row][endCol] = counter;
+            counter++;
+        }
+        endCol--;
+
+        for (let col = endCol; col >= startCol; col--) {
+            results[endRow][col] = counter;
+            counter++;
+        }
+        endRow--;
+
+        for (let row = endRow; row >= startRow; row--) {
+            results[row][startCol] = counter;
+            counter++;
+        }
+        startCol++;
+    }
+
+    return results;
+}
 
 module.exports = matrix;
