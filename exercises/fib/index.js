7b0e6a34a2f6753a4e3de9b02919d7c856665380
gogolab <m4ttek@gmail.com>
2018-09-12 16:09:47 +0200

    finished fib excercise

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,80 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//     const fibSequence = [0, 1];
+
+//     for (let i = 2; i <= n; i++) {
+//         fibSequence.push(fibSequence[i - 1] + fibSequence[i - 2]);
+//     }
+
+//     return fibSequence[n];
+// }
+
+// function fib(n, fibSequence = [0, 1]) {
+//     if (n === 0 || n === 1) {
+//         return fibSequence[n];
+//     }
+
+//     if (fibSequence.length === n + 1) {
+//         return;
+//     }
+
+//     const len = fibSequence.length;
+//     const newItem = fibSequence[len - 2] + fibSequence[len - 1];
+
+//     fibSequence.push(newItem);
+
+//     fib(n, fibSequence);
+
+//     return fibSequence[n];
+// }
+
+// --- start ---------------------------------
+
+// function memoize(fn) {
+//     const cache = {};
+
+//     return function(...args) {
+//         if (cache[args]) {
+//             return cache[args];
+//         }
+
+//         // const result = fn.apply(this, args);
+//         const result = fn(...args);
+
+//         cache[args] = result;
+
+//         return result;
+//     };
+// }
+
+// function fib(n) {
+//     if (n < 2) {
+//         return n;
+//     }
+
+//     return fib(n - 2) + fib(n - 1);
+// }
+
+// fib = memoize(fib);
+
+// --- end ---------------------------------
+
+function fib(n, cache = {}) {
+    if (n < 2) {
+        return n;
+    }
+
+    if (cache[n]) {
+        return cache[n];
+    }
+
+    const result = fib(n - 2, cache) + fib(n - 1, cache);
+
+    cache[n] = result;
+
+    return result;
+}
 
 module.exports = fib;
