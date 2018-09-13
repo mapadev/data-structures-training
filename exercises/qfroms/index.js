88d35f53e17ca34c4d42891ca5d54cb0b4eaff02
gogolab <m4ttek@gmail.com>
2018-09-13 14:56:28 +0200

    finished qfroms excercise

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -12,8 +12,43 @@
 //     q.remove(); // returns 1
 //     q.remove(); // returns 2
 
-const Stack = require('./stack');
+const Stack = require("./stack");
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.stackIn = new Stack();
+        this.stackOut = new Stack();
+    }
+
+    add(record) {
+        this.stackIn.push(record);
+    }
+
+    remove() {
+        this.refill(this.stackIn, this.stackOut);
+
+        const result = this.stackOut.pop();
+
+        this.refill(this.stackOut, this.stackIn);
+
+        return result;
+    }
+
+    refill(src, target) {
+        while (src.peek()) {
+            target.push(src.pop());
+        }
+    }
+
+    peek() {
+        this.refill(this.stackIn, this.stackOut);
+
+        const result = this.stackOut.peek();
+
+        this.refill(this.stackOut, this.stackIn);
+
+        return result;
+    }
+}
 
 module.exports = Queue;
